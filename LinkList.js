d11f6a80c6a3aa106e7f357038b53718d964f0f6
ani213 <vermaaniket213@gmail.com>
2021-12-24 10:51:07 +0530

    created method to add node in the last

--- LinkList.js
+++ LinkList.js
@@ -1,32 +1,36 @@
+// Created a Node object 
 class Node{
     constructor(data,next=null){
         this.data=data;
         this.next=next;
     }
 }
+// Created Linklist object
 class LinkList{
     constructor(){
         this.head=null;
         this.size=0;
     }
+    /*Insert data at First and in head there will be 
+    head:Node {data:data,next: Node {data:data,next: Node{data:next}}}*/
     insertFirst(data){
         this.head=new Node(data,this.head);
     }
+    /* insert at last in head there will be
+     head:Node {data:data,next:Node{data:data,next:{data:data,next:null}}}*/
    insertLast(data){
-    this.head=new Node(data);
-    let current;
        if(!this.head){
+           this.head=new Node(data)
            this.size++;
        }else{
            let current=this.head
            while(current.next){
              current=current.next;
            }
-           this.head=new Node(data,this.head);
+           current.next=new Node(data)
            this.size++;
        }
    }
-
     printList(){
         let current=this.head;
         while(current){
@@ -38,7 +42,9 @@ class LinkList{
 const li= new LinkList();
 li.insertFirst(12);
 li.insertFirst(100);
-li.insertFirst(200)
-li.insertFirst(400)
-li.insertLast(9999)
+li.insertFirst(200);
+li.insertFirst(400);
+li.insertLast(100)
+li.insertLast(200);
+li.insertLast(300)
 li.printList()
